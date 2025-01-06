8aeb4fea0387cdbd40eccb6fd6b1fcad9e5ab9ca
Quan <quanden2222@gmail.com>
2025-01-06 14:49:31 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,39 @@
+Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resources :products
+  root "products#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "/products", to: "products#index"
+  # post "/products", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  # get "/blog/:title", to: "blog#show"
+  # get "/blog/:slug", to: "blog#show"
+
+# get "/products", to: "products#index"
+
+# get "/products/new", to: "products#new"
+# post "/products", to: "products#create"
+
+# get "/products/:id", to: "products#show"
+
+# get "/products/:id/edit", to: "products#edit"
+# patch "/products/:id", to: "products#update"
+# put "/products/:id", to: "products#update"
+
+# delete "/products/:id", to: "products#destroy"
+
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
